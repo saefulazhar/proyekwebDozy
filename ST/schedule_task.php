5b66e5703faace58d60a91657cb20301c4055c71
saefulazhar <saefulazhar.2512033@gmail.com>
2024-11-23 17:31:08 +0700

    menu schedule task

---
+++ ST/schedule_task.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>schedule & task</title>
+</head>
+<body>
+<div class="container">
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <h1>dozy.</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Schedule & Task</a></li>
+                <li><a href="#">Progress</a></li>
+                <li><a href="#">Setting</a></li>
+                <li><a href="#">Log out</a></li>
+            </ul>
+        </div>
+        <div class="content">
+        <div class="header">
+                <h2>Hello, Soffa! Welcome back to dozy.</h2>
+                <img src="https://via.placeholder.com/40" alt="Profile Picture" style="border-radius: 50%;">
+            </div>
+        </div>
+</div>
+</body>
+</html>
